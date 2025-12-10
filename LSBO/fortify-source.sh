0068c9d5772a97fb2fc2ed1bd3e302984bbc4d2e
qv1ntv5 <sanmillan.german@gmail.com>
2025-12-10 14:05:40 +0100

    FORTIFY_SOURCE

--- LSBO/fortify-source.sh
+++ LSBO/fortify-source.sh
@@ -1,4 +1,6 @@
-gcc -D_FORTIFY_SOURCE=3 -Wall -g -O2 test.c -o test # Buffer overflow detection at runtime and compile time.
+# FORTIFY_SOURCE is a preprocessor macro in GCC/Clang compilers framework which helps with BO detection at runtime/compiled-time.
+
+gcc -D_FORTIFY_SOURCE=3 -Wall -g -O2 test.c -o test 
 
 # Is worth to note that there is no difference between a runtime detection and a crash regars to detection.
 # Compile-time detection issues do not englobe overflows with runtime-defined values, only with hardcoded values or compile-time defined ones
